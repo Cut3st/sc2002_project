b7a79c02f27fe436d5f1c66650423c05957f4eb7
Brayden-Choi <goldmerlin9@gmail.com>
2026-04-11 04:20:46 +0800

    Push CLI

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import ui.CLI;
+import combat.GameController;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        CLI cli = new CLI();
+         GameController gc = new GameController(cli);
+        gc.run();
+    }
 }
