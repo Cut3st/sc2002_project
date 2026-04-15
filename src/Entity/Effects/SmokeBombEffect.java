b9d49baed1eb9a349b03d19cee613ce44618b814
Danny <dannyhuang1212@gmail.com>
2026-04-15 18:41:04 +0800

    Revert SmokeBombEffect duration from 3 back to 2

--- src/Entity/Effects/SmokeBombEffect.java
+++ src/Entity/Effects/SmokeBombEffect.java
@@ -9,7 +9,7 @@ import Entity.Combatants.Combatant;
 
 public class SmokeBombEffect extends StatusEffect {
     public SmokeBombEffect() {
-        super("SMOKE_BOMB", 3);
+        super("SMOKE_BOMB", 2);
     }//Define SmokeBombEffect Properties
 
     public void onApply(Combatant smokeUser) {
