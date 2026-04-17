9d3c22ae6b404b2bf771e56e0abb8b01c41670c7
brentwcy <wongchuanyuan94@gmail.com>
2026-04-18 01:02:08 +0800

    block poison curse during smoke bomb

--- src/Entity/EnemyActions/PoisonCurse.java
+++ src/Entity/EnemyActions/PoisonCurse.java
@@ -7,6 +7,11 @@ import Entity.Effects.PoisonEffect;
 public class PoisonCurse implements EnemyAction {
     @Override
     public void execute(Enemy enemy, BattleInfo context) {
+        if (context.isSmokeBombed(context.getPlayer())) {
+            System.out.println(enemy.getName() + " tries to cast Poison Curse, but the smoke bomb blocks the attack!");
+            return;
+        }
+
         context.applyStatusEffect(context.getPlayer(), new PoisonEffect());
         System.out.println(enemy.getName() + " casts Poison Curse on " + context.getPlayer().getName() + ".");
     }
